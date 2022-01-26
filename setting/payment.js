ca1654699080815628d8bb29bd124244ad3f6c3e
Faizofficial18 <faizrahmadani657@gmail.com>
2022-01-26 17:34:25 +0700

    pale

---
+++ setting/payment.js
@@ -0,0 +1,9 @@
+exports.payment = (prefix) => {
+return `╭──「 *PAYMENT / DONASI* 」
+│
+├❏ Gopay : _Tidak Tersedia_
+├❏ Dana : _Tidak Tersedia_
+├❏ Ovo : _Tidak Tersedia_
+│
+╰──`
+}
---
+++ setting/sewabot.js
@@ -0,0 +1,18 @@
+exports.sewabot = (prefix) => {
+return `╭──「 *SEWABOT* 」
+│
+├❏ Bot ini Gratis
+├❏ Joined Group Wa
+├❏https://chat.whatsapp.com/ DSWaMJHACLG97YOprxD17F
+├❏ INVITE BOT
+├❏ wa.me/6287869922719
+├❏ wa.me/6281522680366
+│
+├──「 *PREMIUM* 」
+│
+├❏ 1 MINGGU : 10.000 ( 10K )
+├❏ 1 BULAN : 15.000 ( 15K )
+├❏ PERMANEN : 20.000 ( 20K )
+│
+╰──「 *${botname}* 」`
+}
